3f332e9f517f0462c26725ca9118f184276cf589
RGN91 <rgn91@yandex.ru>
2025-02-27 04:42:04 +0300

    - исполенение команд-функций для следующих кнопок: * кнопка "Удалить"; * кнопка "Добавить дату создания". - обработка некоторых ошибок; - изменение размеров кнопок для функций основного окна.

--- GUI.py
+++ GUI.py
@@ -37,53 +37,109 @@ sys.stdout = OutputRedirector(output_text)
 sys.stderr = OutputRedirector(output_text)
 
 
-#
-def add_doc():
-    pass
+# функции-команды для кнопок
+def add_toc():
+    add_toc_window = tk.Toplevel()
+    add_toc_window.title("Добавление даты создания в название файла или файлов")
+    add_toc_window.geometry("300x200")
+    add_to_file_btn = ttk.Button(add_toc_window, text="Добавить в выбранный файл", command=lambda: add_toc_1())
+    add_to_file_btn.place(relx=0.1, rely=0.1)
+    add_to_files_btn = ttk.Button(add_toc_window, text="Добавить в файлы папки\n(кроме файлов вложенных папок)",
+                                  command=lambda: add_toc_2())
+    add_to_files_btn.place(relx=0.1, rely=0.25)
+    add_to_all_files_btn = ttk.Button(add_toc_window,
+                                      text="Добавить во все файлы папки\n(включая файлы вложенных папок)",
+                                      command=lambda: add_toc_3())
+    add_to_all_files_btn.place(relx=0.1, rely=0.475)
+
+
+def add_toc_1():
+    fof_path = filedialog.askopenfilename()
+    if fof_path:
+        functions.add_time_of_creation_1(fof_path)
+        print(f"Дата создания добавлена в название указанного файла:\n{fof_path}")
+
+
+def add_toc_2():
+    fof_path = filedialog.askdirectory()
+    if fof_path:
+        functions.add_time_of_creation_2(fof_path)
+        print(f"Дата создания добавлена в названия файлов указанной папки:\n{fof_path}")
+
+
+def add_toc_3():
+    fof_path = filedialog.askdirectory()
+    if fof_path:
+        functions.add_time_of_creation_3(fof_path)
+        print(f"Дата создания добавлена в названия файлов указанной папки:\n{fof_path}")
 
 
 # команда для кнопки "Анализ папки"
 def analysis():
     folder_path = filedialog.askdirectory()
-    functions.analysis_of_folder(folder_path)
+    if folder_path:
+        functions.analysis_of_folder(folder_path)
 
 
 # команда для кнопки "Копировать файл"
 def copy_f():
     src_path = filedialog.askopenfilename(title="Выберите файл для копирования")
-    dst_path = filedialog.asksaveasfilename(title="Выберите путь для копии файла, введите имя и тип файла")
-    functions.copy_file(src_path, dst_path)
-    print(f'Файл, расположенный по следующему пути: {src_path} успешно скопирован в указанный путь: {dst_path}')
+    if src_path:
+        dst_path = filedialog.asksaveasfilename(title="Выберите путь для копии файла, введите имя и тип файла")
+        if dst_path:
+            functions.copy_file(src_path, dst_path)
+            print(f'Файл, расположенный по следующему пути: {src_path} успешно скопирован в указанный путь: {dst_path}')
 
 
 # команда для кнопки "Поиск файлов"
 def find_by_reg():
     folder_path = filedialog.askdirectory()
-    find_by_reg_window = tk.Tk()
-    find_by_reg_window.title("Поиск файлов")
-    find_by_reg_window.geometry("250x150")
-    regexp_entry = tk.Entry(find_by_reg_window)
-    regexp_entry.place(relx=0.5, rely=0.1, anchor=tk.N, relwidth=0.8)
-    input_regexp_label = ttk.Label(find_by_reg_window, text="Введите регулярное выражение")
-    input_regexp_label.place(relx=0.5, rely=0.2, anchor=tk.N, relwidth=0.8)
-    find_btn = ttk.Button(find_by_reg_window, text="Найти",
-                          command=lambda: functions.find_by_regex(folder_path, pattern=regexp_entry.get()))
-    find_btn.place(relx=0.5, rely=0.9, anchor=tk.S)
+    if folder_path:
+        find_by_reg_window = tk.Toplevel()
+        find_by_reg_window.title("Поиск файлов")
+        find_by_reg_window.geometry("250x150")
+        regexp_entry = tk.Entry(find_by_reg_window)
+        regexp_entry.place(relx=0.5, rely=0.1, anchor=tk.N, relwidth=0.8)
+        input_regexp_label = ttk.Label(find_by_reg_window, text="Введите регулярное выражение")
+        input_regexp_label.place(relx=0.5, rely=0.2, anchor=tk.N, relwidth=0.8)
+        find_btn = ttk.Button(find_by_reg_window, text="Найти",
+                              command=lambda: functions.find_by_regex(folder_path, pattern=regexp_entry.get()))
+        find_btn.place(relx=0.5, rely=0.9, anchor=tk.S)
 
 
 # команда для кнопки "Количество файлов"
 def nof():
     folder_path = filedialog.askdirectory()
-    functions.number_of_files(folder_path)
+    if folder_path:
+        functions.number_of_files(folder_path)
 
 
 # команда для кнопки "Удалить"
 def remove_fof():
+    rm_window = tk.Toplevel()
+    rm_window.title("Удаление файла или папки")
+    rm_window.geometry("250x150")
+    rm_file_btn = ttk.Button(rm_window, text="Удалить файл", command=lambda: remove_file())
+    rm_file_btn.place(relx=0.1, rely=0.1)
+    rm_folder_btn = ttk.Button(rm_window, text="Удалить папку", command=lambda: remove_folder())
+    rm_folder_btn.place(relx=0.1, rely=0.3)
+
+
+def remove_file():
     fof_path = filedialog.askopenfilename()
-    functions.rm_file_or_folder(fof_path)
+    if fof_path:
+        functions.rm_file_or_folder(fof_path)
+        print(f"Файл по указанному пути:\n{fof_path}\nудален")
 
 
-# кнопки
+def remove_folder():
+    fof_path = filedialog.askdirectory()
+    if fof_path:
+        functions.rm_file_or_folder(fof_path)
+        print(f"Папка по указанному пути:\n{fof_path}\nудалена")
+
+
+# кнопки основного окна
 # кнопка для очистки прокручиваемого текстового виджета
 clear_text_btn = ttk.Button(text="Очистить", command=lambda: output_text.delete("1.0", "end"))
 clear_text_btn.place(relx=0.92, rely=0.54, anchor=tk.NE)
@@ -92,22 +148,22 @@ exit_btn = ttk.Button(text="Выход", command=manager_window.destroy)
 exit_btn.place(relx=0.99, rely=0.99, anchor=tk.SE)
 # кнопка для вызова функции analysis_of_folder
 analysis_btn = ttk.Button(text="Анализ папки", command=lambda: analysis())
-analysis_btn.place(relx=0.5, rely=0.65, anchor=tk.CENTER)
+analysis_btn.place(relx=0.5, rely=0.65, anchor=tk.CENTER, relwidth=0.282)
 # кнопка для вызова функции number_of_files
 nof_btn = ttk.Button(text="Количество файлов", command=lambda: nof())
-nof_btn.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
+nof_btn.place(relx=0.5, rely=0.7, anchor=tk.CENTER, relwidth=0.282)
 # кнопка для вызова функции find_by_regex
 find_by_regex_btn = ttk.Button(text="Поиск файлов", command=lambda: find_by_reg())
-find_by_regex_btn.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
+find_by_regex_btn.place(relx=0.5, rely=0.75, anchor=tk.CENTER, relwidth=0.282)
 # кнопка для вызова функции copy_file
 copy_btn = ttk.Button(text="Копировать файл", command=lambda: copy_f())
-copy_btn.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+copy_btn.place(relx=0.5, rely=0.8, anchor=tk.CENTER, relwidth=0.282)
 # кнопка для вызова функции rm_file_or_folder
-delete_btn = ttk.Button(text="Удалить", command=lambda: remove_fof())
-delete_btn.place(relx=0.5, rely=0.85, anchor=tk.CENTER)
+remove_btn = ttk.Button(text="Удалить", command=lambda: remove_fof())
+remove_btn.place(relx=0.5, rely=0.85, anchor=tk.CENTER, relwidth=0.282)
 # кнопка для вызова группы функций add_time_of_creation
-add_date_of_creation_btn = ttk.Button(text="Добавить дату создания", command=lambda: add_doc())
-add_date_of_creation_btn.place(relx=0.5, rely=0.9, anchor=tk.CENTER)
+add_date_of_creation_btn = ttk.Button(text="Добавить дату создания", command=lambda: add_toc())
+add_date_of_creation_btn.place(relx=0.5, rely=0.9, anchor=tk.CENTER, relwidth=0.282)
 
 manager_window.mainloop()
 sys.stdout = sys.__stdout__
